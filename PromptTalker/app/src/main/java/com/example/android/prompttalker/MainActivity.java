f98e50273cefebbbd6dabae31a5c9c1db38320d9
pkalavath <pranav.kalavath@gmail.com>
2018-03-10 15:53:37 -0800

    update

--- PromptTalker/app/src/main/java/com/example/android/prompttalker/MainActivity.java
+++ PromptTalker/app/src/main/java/com/example/android/prompttalker/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.android.prompttalker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.userSpeech)
+    EditText userSpeech;
+    @BindView(R.id.submitSpeech)
+    Button submitSpeech;
+    @BindView(R.id.speech)
+    TextView DisplaySpeech;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        submitSpeech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String speech = userSpeech.getText().toString();
+                DisplaySpeech.setText(speech);
+            }
+        });
     }
 }
